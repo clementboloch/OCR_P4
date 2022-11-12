8723a50bf990fd5f791fb16c5db50f5544034cad
clementboloch <clement.boloch@gmail.com>
2022-11-12 14:42:22 +0100

    Modify fake data

--- fake_data.py
+++ fake_data.py
@@ -1,24 +1,12 @@
 from Joueur import Joueur
 from Tournoi import Tournoi
+from db_manager import save_data, serialize_object
 
-Joueur()
-Joueur()
-Joueur()
-Joueur()
-Joueur()
-Joueur()
-Joueur()
-Joueur()
-Joueur()
-Joueur()
-Joueur()
-Joueur()
-Joueur()
-Joueur()
-Joueur()
-Joueur()
-Tournoi()
-Tournoi()
-Tournoi()
-Tournoi()
-Tournoi()
+for _ in range(5):
+    NewTournament = Tournoi()
+    serializedNewTournament = serialize_object(NewTournament)
+    save_data('tournaments_table', serializedNewTournament)
+
+    NewPlayer = Joueur()
+    serializedNewPlayer = serialize_object(NewPlayer)
+    save_data('players_table', serializedNewPlayer)
