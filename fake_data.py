e73e6e9e6b38af4182922f03c671ea8c92eba1a1
clementboloch <clement.boloch@gmail.com>
2022-11-18 11:56:11 +0100

    Generate just 1 player and 1 tournament

--- fake_data.py
+++ fake_data.py
@@ -2,11 +2,10 @@ from Joueur import Joueur
 from Tournoi import Tournoi
 from db_manager import save_data, serialize_object
 
-for _ in range(5):
-    NewTournament = Tournoi()
-    serializedNewTournament = serialize_object(NewTournament)
-    save_data('tournaments_table', serializedNewTournament)
+NewTournament = Tournoi()
+serializedNewTournament = serialize_object(NewTournament)
+save_data('tournaments_table', serializedNewTournament)
 
-    NewPlayer = Joueur()
-    serializedNewPlayer = serialize_object(NewPlayer)
-    save_data('players_table', serializedNewPlayer)
+NewPlayer = Joueur()
+serializedNewPlayer = serialize_object(NewPlayer)
+save_data('players_table', serializedNewPlayer)
