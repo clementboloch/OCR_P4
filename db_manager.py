45d4f10c976b58c3430e86a4480b4487b0677f65
clementboloch <clement.boloch@gmail.com>
2022-11-18 17:36:21 +0100

    Transform db_manager in class

--- db_manager.py
+++ db_manager.py
@@ -1,59 +1,45 @@
 from tinydb import TinyDB
-from Tournoi import Tournoi
-from Joueur import Joueur
 
-matchingTable = {'players_table': Joueur, 'tournaments_table': Tournoi}
+class Table:
+    def __init__(self, db_name: str, table_name: str):
+        self.db_name = db_name
+        self.table_name = table_name
 
+        self.db = TinyDB(self.db_name, indent=4, default=str)
+        self.table = self.db.table(self.table_name)
 
-def ask_size(table_name: str):
-    db = TinyDB('db.json', indent=4, default=str)
-    table = db.table(table_name)
-    return len(table)
+    def ask_size(self):
+        return len(self.table)
+    
+    def save_data(self, serialized: dict):
+        self.table.insert(serialized)
 
-def save_data(table_name: str, serialized: dict):
-    db = TinyDB('db.json', indent=4, default=str)
-    table = db.table(table_name)
-    table.insert(serialized)
-
-def import_data_from_id(table_name: str, id: int):
-    db = TinyDB('db.json', indent=4, default=str)
-    table = db.table(table_name)
-    data = table.get(doc_id = id)
-    Object = matchingTable[table_name]
-    Obj = Object(**data)
-    return Obj
+    def import_data_from_id(self, Object: object, id: int):
+        data = self.table.get(doc_id = id)
+        Obj = Object(**data)
+        return Obj
 
 
-def update_data(table_name: str, id: int, updated: dict):
-    db = TinyDB('db.json', indent=4, default=str)
-    table = db.table(table_name)
-    table.update(updated, doc_ids=[id])
+    def update_data(self, id: int, updated: dict):
+        self.table.update(updated, doc_ids=[id])
 
 
-def save_all_data(table_name: str, serialized: list[dict]):
-    db = TinyDB('db.json', indent=4, default=str)
-    table = db.table(table_name)
-    table.truncate()
-    table.insert_multiple(serialized)
+    def save_all_data(self, serialized: list[dict]):
+        self.table.truncate()
+        self.table.insert_multiple(serialized)
 
-def import_all_data(table_name: str, Object: object):
-    db = TinyDB('db.json', indent = 4)
-    table = db.table(table_name)
-    objects = table.all()
+    def import_all_data(self, Object: object):
+        objects = self.table.all()
 
-    created = []
-    for object in objects:
-        created.append(Object(**object))
-    return created
+        created = []
+        for object in objects:
+            created.append(Object(**object))
+        return created
 
+
+# TODO: Mettre dans main
 def serialize_object(object: object):
     return object.__dict__
 
 def serialize(created: list[object]):
     return [object.__dict__ for object in created]
-
-# if __name__ == '__main__':
-#     Tournoi1 = Tournoi('tournoi1')
-#     t = [Tournoi1.__dict__]
-#     save_all_data('tournaments_table', t)
-#     import_all_data('tournaments_table')
